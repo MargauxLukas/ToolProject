045d7e2488e7b5bdfa075e31f0e034c239287e1d
Leithianîn <m.lukas@rubika-edu.com>
2020-10-21 17:38:00 +0200

    Début Gameplay

---
+++ ToolProject/Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BulletMove()
+    {
+        Vector2 pos = transform.position;
+        pos = new Vector2(pos.x, pos.y + speed * Time.fixedDeltaTime);
+        transform.position = pos;
+
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+
+        if (transform.position.y > max.y) Destroy(gameObject);      //later make it move to it start pos
+    }
+}
---
+++ ToolProject/Assets/Scripts/SpaceshipMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipMovement : MonoBehaviour
+{
+    public float speed;
+    public float spriteOffset;
+
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        Vector2 direction = new Vector2(x, 0).normalized;
+        Move(direction);
+    }
+
+    void Move(Vector2 direction)
+    {
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+
+        max.x = max.x - spriteOffset;
+        min.x = min.x + spriteOffset;
+
+        Vector2 pos = transform.position;
+
+        pos += direction * speed * Time.fixedDeltaTime;
+
+        pos.x = Mathf.Clamp(pos.x, min.x, max.x);
+
+        transform.position = pos;
+    }
+}
